3c56318fde62c97e6068358285bbaf3d769271e6
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-11-04 16:56:40 -0300

    Aula como funciona o for e o while

---
+++ Como funciona o For e o While/do_while.js
@@ -0,0 +1,6 @@
+let i = 0
+
+do {
+    console.log('Olá')
+    i++
+} while (i < 3)
---
+++ Como funciona o For e o While/for.js
@@ -0,0 +1,12 @@
+for (let i = 0; i < 4; i++){
+    console.log(i)
+}
+
+let healthPoint = 0
+
+for (let i = 0; i < 10; i++){
+    healthPoint++
+    console.log('Tomou Poção')
+}
+
+console.log("Pontos de vida Totais: "+ healthPoint)
